ba5e2d964e898ac1f4591980b98e9bdec6838754
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-18 10:20:55 -0400

    Add User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :photo, presence: true
+    validates :bio, presence: true
+    validates :posts_counter, presence: true, numericality: { only_integer: true }
+
+    has_many :posts, foreign_key: :author_id
+    has_many :comments, foreign_key: :author_id
+    has_many :likes, foreign_key: :author_id
 end
