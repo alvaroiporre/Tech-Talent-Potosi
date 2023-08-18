ecff895eb779c8733c8bd51dc9ef1da3cda6872b
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-18 10:58:49 -0400

    Add custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,15 @@ class Comment < ApplicationRecord
 
     belongs_to :author, class_name: 'User'
     belongs_to :post
+
+    after_create :update_post_comments_counter
+  before_destroy :update_post_comments_counter_before_destroy
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
+
+  def update_post_comments_counter_before_destroy
+    post.decrement!(:comments_counter)
+  end
 end
