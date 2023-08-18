d3f426d972f31d5ad5bdece18be8ee2615b6cf1c
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-08-18 10:21:10 -0400

    Add Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
-    
+    validates :title, presence: true
+    validates :text, presence: true
+    validates :comments_counter, presence: true, numericality: { only_integer: true }
+    validates :likes_counter, presence: true, numericality: { only_integer: true }
+
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
 end
